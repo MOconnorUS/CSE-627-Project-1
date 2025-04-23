675b92499d1de8fcd4377681ad1517d961fd7566
Matthew <oconnormattc@gmail.com>
2025-04-23 12:35:03 -0400

    Including setup file to install all necessary python files

---
+++ setup.bat
@@ -0,0 +1,7 @@
+python -m pip install numpy
+python -m pip install geopandas
+python -m pip install osmnx
+python -m pip install rasterio
+python -m pip install shapely
+python -m pip install pillow
+python -m pip install tqdm
